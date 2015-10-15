0926b81a866c8d63625a5a4bd3393f6897ca77a9
Ralf Hauptmann <ralf.hauptmann84@gmail.com>
2015-10-14 22:02:04 -0600

    implement functionality to get hour of tweet in users time zone

--- public/api/tweets.php
+++ public/api/tweets.php
@@ -35,13 +35,65 @@ try {
     );
 
     //Make the REST call
-    $data = (array)$cb->$api($params);
+    $tweets = (array)$cb->$api($params);
+    //counts how many tweets checked
+    $tweetsChecked = 0;
 
-    var_dump($data[0]->created_at);die;
+    if($tweets){
+        //init empty list foreach hour
+        $hours = array();
+        for($i = 0; $i < 24; $i++){
+            $hours[sprintf('%02d',$i)] = 0;
+        }
+
+        /**
+         * get user utc offset
+         * As Twitter saves all tweets in GMT and doesn't provide the users time zone foreach tweet
+         * we assume that all tweets are made to the user location/time zone
+         */
+        $userUtcOffset = $tweets[0]->user->utc_offset;
+        $userTimeZoneOffset = null;
+        $subTimeZoneOffset = false;
+
+        if($userUtcOffset){
+            /**
+             * create a DateInterval to add/sub this later foreach tweet,
+             * negative values are not allowed here, thats why we have to save this in $subTimeZoneOffset
+             */
+            $userTimeZoneOffset = new DateInterval('PT'.abs($userUtcOffset).'S');
+            $subTimeZoneOffset = $userUtcOffset < 0;
+        }
+
+        //get hour values foreach tweet
+        foreach($tweets as $tweet) {
+            if(!empty($tweetData->created_at)) {
+                $hour = getHourOfTweet($tweet, $userTimeZoneOffset, $subTimeZoneOffset);
+                $hours[$hour]++;
+                $tweetsChecked++;
+            }
+        }
+        //var_dump($hours);die;
+    }
+
 
     //Output result in JSON, getting it ready for jQuery to process
-    echo json_encode($data);
+    echo json_encode(array(
+        'data' => $hours,
+        'count' => $tweetsChecked
+    ));
+
 }catch(Exception $e){
     var_dump($e->getMessage());
+}
+
+
+function getHourOfTweet($tweetData, DateInterval $timeZoneOffset = null, $subTimeZoneOffset = false){
+    $dateTime = new DateTime($tweetData->created_at);
+
+    if($timeZoneOffset) {
+        $subTimeZoneOffset ? $dateTime->sub($timeZoneOffset) : $dateTime->add($timeZoneOffset);
+    }
+    //echo $tweetData->created_at.' - '.$dateTime->format('H').'<br>';
+    return $dateTime->format('H'); //all times are gmt!
 }
 ?>
